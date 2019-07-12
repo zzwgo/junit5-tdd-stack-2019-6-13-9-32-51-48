7db54a78bf89c5379c706204a13dbeeebbbce049
aaron <kenzzw@163.com>
2019-07-12 11:53:28 +0800

    test when given 5 and call number() will return Buzz. -> Test pass

--- src/main/java/com/thoughtworks/tdd/FizzBuzzWhitzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzWhitzz.java
@@ -3,7 +3,9 @@ package com.thoughtworks.tdd;
 public class FizzBuzzWhitzz {
 
     public static String number(int i){
-
+        if(i%5==0){
+            return "Buzz";
+        }
         return  i+"";
     }
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzWhitzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzWhitzzTest.java
@@ -9,5 +9,8 @@ class FizzBuzzWhitzzTest {
     public void shouldReturn2WhenCallNumberGiven2(){
         assertEquals("2",FizzBuzzWhitzz.number(2));
     }
-
+    @Test
+    public void shouldReturnBuzzWhenCallNumberGiven5(){
+        assertEquals("Buzz",FizzBuzzWhitzz.number(5));
+    }
 }
